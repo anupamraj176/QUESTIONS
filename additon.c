b91317927001a65589b30d2ec77dd97ecd1dcc0c
anupam-demo <anupamraj176@gmail.com>
2023-10-11 11:51:36 +0530

    first commit

---
+++ additon.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int a=5;
+    int b=10;
+    int sum=a+b;
+    printf("Sum of a and b is : %d\n ",sum);
+    return 0;
+}
---
+++ integernum.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    int num=45;
+    printf("The Integer Value is :%d\n",num);
+    return 0;
+}
